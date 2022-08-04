6e9e8e47cb2f83d3f64444fbf0e063a5216729c9
Antonio Alan <tonio.alan07@gmail.com>
2022-08-04 10:04:28 -0300

    creation of put and delete routes, test of rest request and writing of JSON file

--- app.js
+++ app.js
@@ -1,11 +1,19 @@
-const { request, response } = require("express");
 const express = require("express");
-const{randomUUID} =require("crypto")
+const{randomUUID} =require("crypto");
+const fs = require("fs");
 
 const app = express();
 
 app.use(express.json());
-const products = [];
+let products = [];
+
+fs.readFile("products.json","utf-8", (err,data)=>{
+    if(err){
+        console.log(err);
+    }else{
+        products = JSON.parse(data);
+    }
+});
 
 // app.get("/first-route", (request, response)=>{
 //     return response.json({
@@ -36,6 +44,8 @@ app.post("/products", (request, response)=>{
 
     products.push(product);
 
+    productFile();
+
     return response.json(product);
 
 });
@@ -49,5 +59,40 @@ app.get("/products/:id",(request,response)=>{
     const product = products.find((product)=>product.id === id);
     return response.json(product);
 });
+
+app.put("/products/:id",(request,response)=>{
+    const {id} =request.params;
+    const{name,price} = request.body;
+
+    const productIndex = products.findIndex((product)=> product.id === id);
+    products[productIndex] = {
+        ...products[productIndex],
+        name,
+        price,
+    };
+    productFile();
+    return response.json({message: "Product changed successfully"})
+});
+
+app.delete("/products/:id",(request,response)=>{
+    const{id} = request.params;
+    const productIndex = products.findIndex((product)=> product.id === id);
+
+    products.splice(productIndex, 1);
+
+    productFile();
+
+    return response.json({message: "Product removed successfully"});
+});
+
+function productFile(){
+    fs.writeFile("products.json",JSON.stringify(products),(err)=>{
+        if(err){
+            console.log(err);
+        }else{
+            console.log("Product inserted successfully");
+        }
+    });
+}
 
 app.listen(4002, () => console.log("Server is running on port 4002"))
