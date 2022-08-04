0e235fa5f4dc5ca778304b3eddcb54f428b57ff9
Antonio Alan <tonio.alan07@gmail.com>
2022-08-03 22:51:19 -0300

    tests to create serve with message in JSON

--- serve.js
+++ serve.js
@@ -1,4 +1,13 @@
+//Criando o server
 const http = require("http");
 
-http.createServer((request, response)=>{})
+http.createServer((request, response)=>{
+    response.writeHead(200,{"Content-Type":"application/json"});
+
+    response.end(
+        JSON.stringify({
+            message:"my first application with node js",
+        })
+    );
+})
 .listen(4001,()=> console.log("Server is running on port 4001"));
