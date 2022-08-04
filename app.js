6d50d5a837e659aaaedd8c4178b79f86f0eddd1c
Antonio Alan <tonio.alan07@gmail.com>
2022-08-03 23:39:39 -0300

    nodemon installation

--- app.js
+++ app.js
@@ -1,5 +1,12 @@
+const { request, response } = require("express");
 const express = require("express");
 
 const app = express();
+
+app.get("/first-route", (request, response)=>{
+    return response.json({
+        message: "Accessed first route with nodemon",
+    });
+});
 
 app.listen(4002, () => console.log("Server is running on port 4002"))
